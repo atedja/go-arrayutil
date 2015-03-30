77f4e309465d7f0ba3ed1c70be52de2e2a7841bb
Albert Tedja <albert@alberttedja.com>
2015-03-29 22:40:27 -0700

    Updated documentation

--- map.go
+++ map.go
@@ -2,9 +2,16 @@ package arrayutil
 
 type MapFunc func(interface{}) interface{}
 
-/**
-Invokes func for each element in the array. Creates a new array containing
-the values returned by the func.
+/*
+Invokes `mapFunc` for each element in the array. Creates a new array containing
+the values returned by `mapFunc`
+Example:
+
+		func MyMapFunc(v interface{}) interface{} {
+			return v.(int) * 3
+		}
+		var myArray = []interface{}{1, 2, 3, 4}
+    result := arrayutil.Map(myArray, MyMapFunc) // [3, 6, 9, 12]
 */
 func Map(arr []interface{}, mapFunc MapFunc) []interface{} {
 	result := make([]interface{}, 0)
